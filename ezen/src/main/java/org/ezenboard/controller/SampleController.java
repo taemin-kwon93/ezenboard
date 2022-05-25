1460a0c46cb0395ed143b8150be4de19fafbf729
DESKTOP-MTGVP5G\DELL <4146kt@gmail.com>
2022-05-25 18:08:33 +0900

    Sample 삭제

--- ezen/src/main/java/org/ezenboard/controller/SampleController.java
+++ ezen/src/main/java/org/ezenboard/controller/SampleController.java
@@ -1,6 +1,5 @@
 package org.ezenboard.controller;
 
-
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -14,35 +13,37 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SampleController {
 	
-	  @GetMapping("/all")
-	  public void doAll() {
-	    
-	    log.info("do all can access everybody");
-	  }
-	  
-	  @GetMapping("/member")
-	  public void doMember() {
-	    
-	    log.info("logined member");
-	  }
-	  
-	  @GetMapping("/admin")
-	  public void doAdmin() {
-	    
-	    log.info("admin only");
-	  }  
-	  
-	  @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MEMBER')")
-	  @GetMapping("/annoMeber")
-	  public void doMember2() {
-		  log.info("logined annotation member");
-	  }
-	  
-	  @Secured({"ROLE_ADMIN"})
-	  @GetMapping("/annoAdmin")
-	  public void doAdmin2() {
-		  log.info("admin annotation only");
-	  }
-	   	
-	  
+	//22.05.25
+	//제작 완료되면 해당 컨트롤러 지울것.
+	
+	@GetMapping("/all")
+	public void doAll() {
+
+		log.info("do all can access everybody");
+	}
+
+	@GetMapping("/member")
+	public void doMember() {
+
+		log.info("logined member");
+	}
+
+	@GetMapping("/admin")
+	public void doAdmin() {
+
+		log.info("admin only");
+	}
+
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MEMBER')")
+	@GetMapping("/annoMeber")
+	public void doMember2() {
+		log.info("doMember2 _ logined annotation member");
+	}
+
+	@Secured({ "ROLE_ADMIN" })
+	@GetMapping("/annoAdmin")
+	public void doAdmin2() {
+		log.info("admin annotation only");
+	}
+
 }
